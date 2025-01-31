1b08ef898f0fec9accf429400a9873f365ddaf92
Saiful Islam <saiful.abir20@gmail.com>
2025-01-31 11:24:21 +0100

    card ui for startup is done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,21 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date,
+      views: 55,
+      author: { _id: 1, name: 'Adrian' },
+      _id: 1,
+      description: "This is a description",
+      image: "https://www.epitech-it.es/wp-content/uploads/2022/07/danial-igdery-FCHlYvR5gJI-unsplash.jpg",
+      category: "Robots",
+      title: "We Robots"
+    }
+  ]
   return (
     <>
       <section className="pink_container">
@@ -10,6 +24,20 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
